070f1d6dabef2ff44a96579943c0aff29fae7800
golang king <lukjanov.nikita-luk@yandex.ru>
2025-04-16 16:07:34 +0300

    order update 1.2.6

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 const API_CONFIG = {
-    BASE_URL: 'http://localhost:8080', // URL локального бэкенда
+    BASE_URL: 'https://pc-tech-shop-1-backend.onrender.com', // URL локального бэкенда
     ENDPOINTS: {
         AUTH: {
             LOGIN: '/login',
