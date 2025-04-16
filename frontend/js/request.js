a3b08cc5b3241cf7b29c5c209e337517b12bf962
golang king <lukjanov.nikita-luk@yandex.ru>
2025-04-16 13:38:44 +0300

    Fix requests

--- frontend/js/request.js
+++ frontend/js/request.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch(buildApiUrl('/help-request'), {
+                const response = await fetch(buildApiUrl('/requests'), {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
